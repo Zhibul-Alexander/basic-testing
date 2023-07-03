4f81d1ce63e621818a540fde579e6d56403841ed
Alexander Zhibul <zhibul.alexander@gmail.com>
2023-07-04 00:22:16 +0400

    feat: add tests for 07-mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,25 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import { throttledGetDataFromApi } from "./index";
 
-describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    // Write your test here
+describe("throttledGetDataFromApi", () => {
+  test("should create instance with provided base url", async () => {
+    const responseData = await throttledGetDataFromApi("/posts");
+
+    expect(Array.isArray(responseData)).toBe(true);
   });
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
+  test("should perform request to correct provided url", async () => {
+    const responseData = await throttledGetDataFromApi("/posts/1");
+    if (responseData) {
+      expect(responseData[0].id).toBe(1);
+      expect(typeof responseData[0].title).toBe("string");
+      expect(typeof responseData[0].body).toBe("string");
+    }
   });
 
-  test('should return response data', async () => {
-    // Write your test here
+  test("should return response data", async () => {
+    const responseData = await throttledGetDataFromApi("/comments");
+
+    expect(Array.isArray(responseData)).toBe(true);
   });
 });
